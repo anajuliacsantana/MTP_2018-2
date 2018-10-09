c3b7402ebde87522b05df604cc4947d0b1379dac
Ana Júlia <anajulia.costasantana@gmail.com>
2018-10-09 05:36:57 -0300

    Adiciona P3.c

---
+++ P3.c
@@ -0,0 +1,24 @@
+/*P3.c*/
+/* Ana Julia Costa Santana */
+/* 11811ETE003*/
+#include <stdio.h>
+
+int main()
+{
+	int i,cont=0;
+	char numero[256];
+	printf("\nInsira uma sequencia:");
+	scanf("%s",numero);
+	printf("%s\n",numero);
+	for(i=0;numero[i]!='\0'; i++)
+	{
+       if(numero[i] >='0' && numero[i] <='9' ) 
+       {
+       	printf("%d",numero[i]-'0');
+       	cont++;
+       }
+	}
+	if(cont==0)
+	printf("0");
+	return 0;
+}
