821ebe4ba35719fff105b8f36bb011550a735b0f
Ana Júlia Santana <42445665+anajuliacsantana@users.noreply.github.com>
2018-11-07 22:30:39 -0200

    Add P4.c

---
+++ p4.c
@@ -0,0 +1,27 @@
+﻿/*P4.c*/
+/* Ana Julia Costa Santana */
+/* 11811ETE003*/
+
+#include <stdio.h>
+
+int A(int,int );
+int main(){
+	int m,n;
+	printf("Insira valor para m e n , de espaco:\n");
+	scanf("%d%d",&m,&n);
+	printf("%d",A(m,n));		
+	return 0;
+	}
+	
+int A(int m,int n)
+	{
+		if(m== 0)
+		return n+1;
+		else if(n==0 && m>0)
+		return A(m-1,1);
+		else
+        return A(m - 1, A(m, n - 1));
+		}
+		
+
+
